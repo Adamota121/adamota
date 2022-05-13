59e9ef1bc6e7272285d24eb6ea8c4ed829a11a5f
СГК ГАПОУ <sgk.gapou@outlook.com>
2022-05-13 13:47:01 +0400

    Добавьте файлы проекта.

---
+++ Diskriminant/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Diskriminant
+{
+    internal class Program
+    {
+        public static decimal Main()
+        {
+            Console.WriteLine("Введите число a");
+            decimal a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            decimal b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            decimal c = Convert.ToInt32(Console.ReadLine());
+
+            return Disk();
+        }
+
+        public static decimal Disk()
+        {
+            decimal D = b**2 - 4 * a * c;
+            return D;
+        }
+    }
+}
